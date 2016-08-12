44da6ccb0e64a1d8ff1680b27d5865a5c0387586
HK Kahng <hk@router-rooter.com>
2016-08-12 10:52:40 -0700

    add function to display description of selected track

--- js/scripts.js
+++ js/scripts.js
@@ -54,9 +54,18 @@ var findTrack = function(myScores) {
 var trackName = function(highScore) {
   var programTracks = ["Ruby/Rails", "PHP/Drupal", "Java/Android", "CSS/Design", "C#/.Net"]
   return programTracks[highScore - 1];
-}
+};
 // This function returns the description of the track with the highest score
-
+var trackDescription = function(highScore) {
+  var programDescriptions = [
+    "Ruby and Rails are typically used by younger companies and startups, especially for building interactive web applications.",
+    "PHP and Drupal are typically used to build content-focused websites.",
+    "Java is one of the most widely-used programming languages, and is used in everything from Android apps to large enterprise development.",
+    "The CSS/Design track is a launching point for anyone interested in designing beautiful, memorable web experiences.",
+    "C# and .NET are typically used by larger enterprises, government agencies, and companies serving enterprise and government clients."
+  ]
+  return programDescriptions[highScore - 1];
+};
 
 // Interface Logic
 $(document).ready(function() {
@@ -75,14 +84,17 @@ $(document).ready(function() {
     var yourScores = myScores(yourInput);
     var yourTrack = findTrack(yourScores);
     var yourTrackName = trackName(yourTrack);
+    var yourTrackDescription = trackDescription(yourTrack);
 
     $("#yourDestiny").text(yourTrackName);
+    $("#resultText").text(yourTrackDescription);
     for (i = 1; i < 6; i++) {
       $("#scoreTrack" + i).text(yourScores[i - 1]);
     }
 
     $("#myResults").show();
     $("#resultImage" + yourTrack).show();
+    $("#disclaimer").show();
 
     event.preventDefault();
   });
